e65281aca245ea672c888d350892a544ed996c1a
Eugene Che <eugeneyche@gmail.com>
2014-04-05 01:36:56 -0700

    Inclusive spill

--- cpp/include/spill_tree.h
+++ cpp/include/spill_tree.h
@@ -42,7 +42,7 @@ KDTreeNode<Label, T> * SpillTree<Label, T>::build_tree(size_t c, double a,
     vector<size_t> subdomain_r;
     for (size_t i = 0; i < domain.size(); i++)
     {
-        if (pivot_l < values[i] && values[i] <= pivot_r)
+        if (pivot_l <= values[i] && values[i] <= pivot_r)
         {
             if (subdomain_l.size() < subdomain_l_lim)
                 subdomain_l.push_back(domain[i]);
